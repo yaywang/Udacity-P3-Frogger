6633d70ce7e7248e4f9cee7bf4194a097898f916
Ye Wang <Ye@Yes-MacBook.local>
2016-02-08 16:18:51 +0800

    Append the canvas to canvas div
    In the beginning, canvas was appended to the end of main. This means that the you could not add a div after main.

--- js/engine.js
+++ js/engine.js
@@ -27,7 +27,7 @@ var Engine = (function(global) {
 
     canvas.width = 505;
     canvas.height = 606;
-    doc.body.getElementsByClassName('main')[0].appendChild(canvas);
+    doc.body.getElementsByClassName('canvas')[0].appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
      * and handles properly calling the update and render methods.
