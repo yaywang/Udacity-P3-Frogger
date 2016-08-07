436db1cf469ebf6d769c79503d162b974adb48c0
Ye <wangyegp@gmail.com>
2016-08-07 13:27:45 +0300

    Implement enemies' speed variation mechanism
    Now enemies come in at different speeds, a faster one would stop
    behind a slow one.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var global = this;
+var global = this,
+    allEnemies = [];
 
 // The scoring mechanism
 var score = {};
@@ -24,30 +25,55 @@ lives.update(0);
 
 // Enemies our player must avoid
 var Enemy = function() {
-    // Initial positions are randomized, to a few enemies always appear new
-    this.x = -100 * Math.random();
-    // Randomly assign the three possible y values: 61.5, 144.5, 227.5
-    this.y = 61.5 + 83 * Math.floor(Math.random() * 3);
+    // Boundaries:
+    this.leftBound = -101;
+    this.rightBound = 505;
+    // Randomly output x and y values:
+    this.initX = function() {
+        return this.leftBound - 101 * Math.floor(Math.random() * 42);
+    };
+    this.initY = function() {
+        return 61.5 + 83 * Math.floor(Math.random() * 3);
+    };
+
+    this.x = this.initX();
+    this.y = this.initY();
+
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.initialSpeed = 100 + 100 * Math.random();
 
-    this.speed = 100 * (Math.random() * 300);
+    // Generate speed
+    this.updateSpeed = function() {
+        this.speed = this.initialSpeed;
+        for (var i = 0; i < allEnemies.length; i++) {
+            var xDistance = allEnemies[i].x - this.x;
+            if (this.y == allEnemies[i].y && xDistance >= 0 && xDistance < 101) {
+                this.speed = allEnemies[i].speed;
+            }
+        }
+    };
 };
- 
+
+
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.updateSpeed();
+
     this.x += this.speed * dt;
+
+    //checkCollapsedBugs();
+
     // Randomize and assign the new initial positions
     // when an enemy hits the right boundary
-    if (this.x >= 550) {
-        this.x = -1000 * Math.random();
-        // Randomly assign the three possible values: 61.5, 144.5, 227.5
-        this.y = 61.5 + 83 * Math.floor(Math.random() * 3);
+    if (this.x >= this.rightBound) {
+        this.x = this.initX();
+        this.y = this.initY();
     }
 };
 
@@ -79,14 +105,14 @@ Player.prototype.checkBoundaries = function() {
         score.update(100);
         this.y = 415;
     }
-    
+
     if (this.y == 498) {
         this.y = 415;
-    } 
+    }
 
     if (this.x == -101) {
         this.x = 0;
-    } 
+    }
 
     if (this.x == 505) {
         this.x = 404;
@@ -111,16 +137,27 @@ Player.prototype.handleInput = function(key) {
 };
 
 // Instantiate all enemies and the player
-var allEnemies = [];
-for (var i = 0; i < 10; i++) {
+for (var i = 0; i < 22; i++) {
     var newEnemy = new Enemy();
     allEnemies.push(newEnemy);
 }
 
-// // TODO: the speed variation algorithm
-// allEnemies.forEach(function(enemy) {
-//     enemy.x;
-// })
+function checkCollapsedBugs() {
+    for (var i = 0; i < allEnemies.length; i++) {
+        //console.log('Entering for loop in checkCollaspsedBugs');
+        if (allEnemies[i].x <= 0) {
+            for (var j = 0; j < allEnemies.length; j++) {
+                if (allEnemies[j].x <= 0) {
+                    var xDist = allEnemies[j] - allEnemies[i];
+                    if (allEnemies[i].y == allEnemies[j].y && xDist >= 0 && xDist <= 101) {
+                        allEnemies[i].x = allEnemies[j].x;
+                        // console.log('x changed');
+                    }
+                }
+            }
+        }
+    }
+}
 
 var player = new Player();
 
@@ -160,20 +197,20 @@ function checkCollisions(player) {
     // Drop player to initial position and lose one life once it hits any enemy
     for (var i = 0; i < allEnemies.length; i++) {
         var enemy = allEnemies[i];
-        var disX = player.x - enemy.x;
-        var disY = player.y - enemy.y;
-        if (disX <= 50 && disY <= 50 && disX >= -50 && disY >= -50) {
+        var disToEnemyX = player.x - enemy.x;
+        var disToEnemyY = player.y - enemy.y;
+        if (disToEnemyX <= 50 && disToEnemyY <= 50 && disToEnemyX >= -50 && disToEnemyY >= -50) {
             lives.update(-1);
             player.y = 415;
         }
     }
 
-    for (var i = 0; i < allGems.length; i++) {
-        var gem = allGems[i];
+    for (var j = 0; j < allGems.length; j++) {
+        var gem = allGems[j];
         if (gem.endTime > Date.now()) {
-            var disX = player.x - gem.x;
-            var disY = player.y - gem.y;
-            if (disX <= 50 && disY <= 50 && disX >= -50 && disY >= -50) {
+            var disToGemX = player.x - gem.x;
+            var disToGemY = player.y - gem.y;
+            if (disToGemX <= 50 && disToGemY <= 50 && disToGemX >= -50 && disToGemY >= -50) {
                 // Reward for getting a gem
                 score.update(10);
                 // Set the endTime to now so the gem will disappear at the next frame
